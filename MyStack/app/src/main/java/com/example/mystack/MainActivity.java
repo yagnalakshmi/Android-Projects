1a1e48c9c794b0ae183c94ed0a1bfc70dac65753
YagnalakshmiSomayajulu <yagnalakshmis@gmail.com>
2019-04-17 14:51:47 +0800

    Initial commit

---
+++ MyStack/app/src/main/java/com/example/mystack/MainActivity.java
@@ -0,0 +1,90 @@
+
+package com.example.mystack;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class SetupFetch extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            URL url;
+            String page = "";
+            HttpsURLConnection urlConnection = null;
+
+
+            try {
+                url = new URL(strings[0]);
+
+                urlConnection = (HttpsURLConnection) url.openConnection();
+
+                InputStream input = urlConnection.getInputStream();
+
+                InputStreamReader read = new InputStreamReader(input);
+
+
+                int data = read.read();
+
+                while (data != -1){
+                    char storeData = (char) data;
+                    page += storeData;
+                    data = read.read();
+                }
+                return page;
+
+
+            }
+
+            catch(Exception e) {
+                e.printStackTrace();
+                return "Cannot fetch web page";
+            }
+
+
+
+
+        }
+    }
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SetupFetch request = new SetupFetch();
+
+        String page = null;
+
+
+        try {
+            page = request.execute("https://www.google.com/").get();
+            Log.i("DONE", "DONE");
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Log.i("Stackoverflow", page);
+        Log.i("DONE", "DONE");
+
+    }
+}
+
